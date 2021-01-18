c73566a9514b1a3e05e7b6250ea28cd4c0738886
bob181195 <75467651+bob181195@users.noreply.github.com>
2021-01-18 10:43:28 +0530

    added config prop
    config file

--- src/main/java/StepDefn/PO.java
+++ src/main/java/StepDefn/PO.java
@@ -1,7 +1,11 @@
 package StepDefn;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -10,18 +14,23 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
+
 public class PO {
-public void login() {
+public void login() throws IOException {
     // Write code here that turns the phrase above into concrete actions
 	WebDriver driver;
 Map<String, Object> prefs = new HashMap<String, Object>();
 prefs.put("profile.default_content_setting_values.notifications", 2); 
 ChromeOptions options = new ChromeOptions();
 options.setExperimentalOption("prefs", prefs);
- driver = new ChromeDriver(options);
+Properties prop=new Properties();
+FileInputStream ip= new FileInputStream("src/config.properties");
+prop.load(ip);
+driver = new ChromeDriver(options);
 driver.manage().window().maximize();
-driver.get("https://ping.telebu.com/login");
-driver.findElement(By.id("number")).sendKeys("9010358767");
+driver.get(prop.getProperty("URL"));
+driver.findElement(By.id("number")).sendKeys(prop.getProperty("mobileNo"));
 driver.findElement(By.xpath("//button[contains(text(),'Login')]")).click();
 driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 WebElement title = driver.findElement(By.xpath("//*[@title='Telebu']"));
